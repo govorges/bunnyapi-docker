629ea8242ec0b70986248d19a45023127d0a3b79
gianaut <157207223+gianaut@users.noreply.github.com>
2024-09-12 01:25:28 -0400

    Initial commit

---
+++ API.py
@@ -0,0 +1,162 @@
+from flask import Flask, request, make_response, jsonify
+from Bunny import BunnyHandler
+from os import path
+
+import time
+from threading import Thread
+from os import remove
+
+HOME_DIR = path.dirname(path.realpath(__file__))
+UPLOAD_DIR = path.join(HOME_DIR, "uploads")
+
+api = Flask(__name__)
+bunny = BunnyHandler()
+
+class UploadWorker:
+    def __init__(self, id, local_file_path = None, target_file_path = None) -> None:
+        self.id = id
+        self.local_file_path = local_file_path
+        self.target_file_path = target_file_path
+        self.time_start = time.time()
+        self.time_end = None
+        
+        self.workerThread = Thread(target=self._target, daemon=True)
+
+    def _target(self):
+        if self.id is not None:
+            bunny.bunny_UploadFile(
+                local_file_path = path.join(UPLOAD_DIR, f"{self.id}.png"),
+                target_file_path = f"/videos/{self.id}.png",
+                content_type = "application/octet-stream"
+            )
+            remove(path.join(UPLOAD_DIR, f"{self.id}.png"))
+
+            bunny.bunny_UploadFile(
+                local_file_path = path.join(UPLOAD_DIR, f"{self.id}.mp4"),
+                target_file_path = f"/videos/{self.id}.mp4",
+                content_type = "application/octet-stream"
+            )
+            remove(path.join(UPLOAD_DIR, f"{self.id}.mp4"))
+        elif self.local_file_path is not None and self.target_file_path is not None:
+            bunny.bunny_UploadFile(
+                local_file_path = self.local_file_path,
+                target_file_path = self.target_file_path,
+                content_type = "application/octet-stream"
+            )
+            if ".xml" in self.target_file_path:
+                bunny.bunny_PurgeLinkCache(f"https://openbroadcast.b-cdn.net/{self.target_file_path}")
+
+
+        self.time_end = time.time()
+    
+
+class UploadQueue:
+    def __init__(self):
+        self.ActiveUploadWorkers = []
+        self.UploadStats = {}
+
+        self.poller = Thread(target=self.WorkerPoll, daemon=True).start()
+
+    def CreateUploadWorker(self, id, local_file_path = None, target_file_path = None):
+        worker = UploadWorker(id=id, local_file_path=local_file_path, target_file_path=target_file_path)
+        self.ActiveUploadWorkers.append(worker)
+        worker.workerThread.start()
+
+    def CheckWorkerStatuses(self):
+        fileList = bunny.bunny_ListFiles("videos/")
+        fileNames = [x["ObjectName"].rsplit(".", 1)[0] for x in fileList]
+
+        for worker in self.ActiveUploadWorkers:
+            if worker.id in fileNames: # Cleanup
+                self.UploadStats[worker.id] = {
+                    "time_start": worker.time_start,
+                    "time_end": worker.time_end
+                }
+                self.ActiveUploadWorkers.remove(worker)
+                del worker
+
+    def WorkerPoll(self):
+        while True:
+            time.sleep(300)
+            self.CheckWorkerStatuses()
+
+uploadQueue = UploadQueue()
+
+@api.route("/status", methods=["GET"])
+def status():
+    return f"{str(uploadQueue.UploadStats)}"
+
+@api.route("/files/upload", methods=["POST"])
+def files_upload_POST():
+    id = request.headers.get("id")
+    if id is None or id == "" or len(id) > 20:
+        return make_response("Header \"id\" is not set or is set incorrectly.", 400)
+
+    uploadQueue.CreateUploadWorker(id=id)
+
+    return make_response("Video is queued to be uploaded.", 200)
+
+@api.route("/files/misc_upload", methods=["POST"])
+def files_misc_upload_POST():
+    local_file_path = request.headers.get("local-file-path")
+    if local_file_path is None or local_file_path == "":
+        return make_response("Header \"local-file-path\" is not set or is set incorrectly.", 400)
+
+    target_file_path = request.headers.get("target-file-path")
+    if target_file_path is None or target_file_path == "":
+        return make_response("Header \"target-file-path\" is not set or is set incorrectly.", 400)
+    
+    uploadQueue.CreateUploadWorker(id = None, local_file_path=local_file_path, target_file_path=target_file_path)
+
+    return make_response("Success", 200)
+
+
+@api.route("/files/upload", methods=["GET"])
+def files_upload_GET():
+    id = request.headers.get("id")
+    if id is None or id == "" or len(id) > 20:
+        return make_response("Header \"id\" is not set or is set incorrectly.", 400)
+
+    if uploadQueue.UploadStats.get(id) is None:
+        return make_response(f"ID \"{id}\" was not found in the upload stats. This is likely due to the upload worker being too old to remain cached.")
+
+    return jsonify(uploadQueue.UploadStats.get(id))
+
+@api.route("/files/list", methods=["GET"])
+def files_list():
+    path = request.headers.get("path", "videos/")
+    if path == "" or len(path) > 30:
+        return make_response("Header \"path\" is not set or is set incorrectly.", 400)
+
+    fileList = bunny.bunny_ListFiles("videos/")
+
+    return jsonify(fileList)
+
+@api.route("/files/delete", methods=["DELETE"])
+def files_delete():
+    target_file_path = request.headers.get("target-file-path")
+    if target_file_path is None or target_file_path == "" or len(target_file_path) > 100:
+        return make_response("Header \"target_file_path\" is not set or is set incorrectly.", 400)
+
+    bunny.bunny_DeleteFile(target_file_path=target_file_path)
+
+    return make_response("Success", 200)
+
+@api.route("/files/retrieve", methods=["GET"])
+def files_retrieve():
+    target_file_path = request.headers.get("target-file-path")
+    if target_file_path is None or target_file_path == "" or len(target_file_path) > 100:
+        return make_response("Header \"target-file-path\" is not set or is set incorrectly.", 400)
+
+    fileData = bunny.bunny_GetFileData(target_file_path=target_file_path)
+    return jsonify(fileData)
+
+@api.route("/cache/purge", methods=["GET"])
+def cache_purge():
+    target_url = request.args.get("url")
+    if target_url is None or target_url == "":
+        return make_response("Argument \"url\" is not set or is set incorrectly.", 400)
+    
+    bunny.bunny_PurgeLinkCache(target_url)
+
+    return make_response(f"Cache purge of {target_url} successful", 200)
---
+++ Bunny.py
@@ -0,0 +1,72 @@
+import requests
+import os
+
+class BunnyHandler:
+    def __init__(self):
+        self.bunny_StorageZone_API_Key = os.environ["BUNNY_STORAGEZONE_KEY"]
+        self.bunny_Account_API_Key = os.environ["BUNNY_ACCOUNT_KEY"]
+
+        if os.getenv("BUNNY_STORAGEZONE") is not None and os.getenv("BUNNY_STORAGEZONE") != "":
+            self.bunny_Region = f'{os.environ["BUNNY_STORAGEZONE"]}.'
+        else:
+            self.bunny_Region = ""
+        self.bunny_StorageZoneName = "openbroadcast-public"
+        self.bunny_StorageZoneEndpoint = "https://" + self.bunny_Region + "storage.bunnycdn.com" + "/" + self.bunny_StorageZoneName
+
+        self.uploadreached = False
+
+        if not self.bunny_ConnectionAlive():
+            raise SystemError("Bunny connection has failed! (NOT AUTH RELATED)")
+
+    def bunny_ConnectionAlive(self):
+        requestURL = "https://api.bunny.net/region"
+        r = requests.get(requestURL)
+
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+
+    def bunny_UploadFile(self, local_file_path, target_file_path, content_type):
+        requestHeaders = {
+            "AccessKey": self.bunny_StorageZone_API_Key,
+            "Content-Type": content_type,
+            "accept": "application/json",
+        }
+        requestURL = self.bunny_StorageZoneEndpoint + target_file_path
+        
+        requests.put(requestURL, data=open(local_file_path, "rb"), headers=requestHeaders)
+    def bunny_ListFiles(self, path: str):
+        requestHeaders = {
+            "AccessKey": self.bunny_StorageZone_API_Key,
+            "accept": "application/json"
+        }
+        requestURL = self.bunny_StorageZoneEndpoint + "/" + path
+
+        fileList = requests.get(requestURL, headers=requestHeaders).json()
+        return fileList
+
+    def bunny_DeleteFile(self, target_file_path):
+        requestHeaders = {
+            "AccessKey": self.bunny_StorageZone_API_Key,
+            "accept": "application/json"
+        }
+        requestURL = self.bunny_StorageZoneEndpoint + target_file_path
+
+        requests.delete(requestURL, headers=requestHeaders)
+
+    def bunny_PurgeLinkCache(self, url):
+        requestHeaders = {
+            "AccessKey": self.bunny_Account_API_Key
+        }
+        requestURL = f"https://api.bunny.net/purge?url={url}"            
+        requests.post(requestURL, headers=requestHeaders)
+    
+    def bunny_GetFileData(self, target_file_path):
+        folderData = self.bunny_ListFiles(target_file_path.rsplit("/", 1)[0])
+        for item in folderData:
+            if item["ObjectName"] == target_file_path.rsplit('/', 1)[-1]:
+                fileData = item
+                break
+
+        return fileData
